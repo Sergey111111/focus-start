c4cdba070c84991f19ec7221af7e1e064d3f9112
kolumb <kokocik3@bk.ru>
2019-11-18 19:12:25 +0300

    tests added

--- src/test/java/mocks/crud/task/service/AddressServiceTest.java
+++ src/test/java/mocks/crud/task/service/AddressServiceTest.java
@@ -1,7 +1,74 @@
 package mocks.crud.task.service;
 
-import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+import mocks.crud.task.model.Address;
+import mocks.crud.task.repository.CrudRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+import static mocks.crud.task.service.TestUtils.TEST_ADDRESS;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AddressServiceTest {
 
+    @Mock
+    CrudRepository<Long, Address> addressRepository;
+
+    private  AddressService addressService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        addressService = new AddressService(addressRepository);
+    }
+
+    @Test
+    public void save() {
+        AddressService spy = spy(new AddressService(addressRepository));
+        spy.save(TEST_ADDRESS);
+        spy.save(TEST_ADDRESS);
+        verify(addressRepository, times(2)).save(TEST_ADDRESS);
+    }
+
+    @Test
+    public void findById() {
+        when(addressRepository.findById(3L)).thenReturn(TEST_ADDRESS);
+        Address result = addressService.findById(3L);
+        assertEquals(result, TEST_ADDRESS);
+        verify(addressRepository, times(1)).findById(3L);
+    }
+
+    @Test
+    public void findAll() {
+        List<Address> testListAddress = new ArrayList<>();
+        testListAddress.add(TEST_ADDRESS);
+        testListAddress.add(TEST_ADDRESS);
+        testListAddress.add(TEST_ADDRESS);
+        when(addressRepository.findAll()).thenReturn(testListAddress);
+        List<Address> result = addressService.findAll();
+        assertEquals(result, testListAddress);
+        verify(addressRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void update() {
+        when(addressRepository.update(TEST_ADDRESS)).thenReturn(TEST_ADDRESS);
+        Address result = addressService.update(TEST_ADDRESS);
+        result = addressService.update(TEST_ADDRESS);
+        assertEquals(result, TEST_ADDRESS);
+        verify(addressRepository, times(2)).update(TEST_ADDRESS);
+    }
+
+    @Test
+    public void delete() {
+        AddressService spy = spy(new AddressService(addressRepository));
+        spy.delete(TEST_ADDRESS);
+        verify(addressRepository, times(1)).delete(TEST_ADDRESS);
+    }
 }
---
+++ src/test/java/mocks/crud/task/service/TestUtils.java
@@ -0,0 +1,7 @@
+package mocks.crud.task.service;
+
+import mocks.crud.task.model.Address;
+
+public class TestUtils {
+    public static final Address TEST_ADDRESS = new Address(3L,"St. Petersburg");
+}
